d08f43fb2ddc123f5869c8a517f9c36ce01fe0c3
th0mat <th.natter@gmail.com>
2016-10-04 11:04:44 +0700

    mac input validation added

--- app/pages/search.js
+++ app/pages/search.js
@@ -6,50 +6,85 @@ import React from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux';
 import {browserHistory} from 'react-router';
-
+import {Button} from 'react-bootstrap';
 
 import Navbar from '../components/navbar';
 
-@connect((store)=>{
+@connect((store)=> {
     return {
         // keep to get this.props.dispatch
     }
 })
 export default class Search extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            searchTarget: 'abcdef123456'
+            searchTarget: 'abcdef123456',
+            macValid: true
         }
     }
 
-    lookupMacHistory(event){
+    lookupMacHistory(event) {
         this.props.dispatch({type: "SET_RETURN_TO_LINK", payload: "/search"});
-        browserHistory.push('/history/' + this.state.searchTarget)
-    };
+        browserHistory.push('/history/' + this.strip(this.state.searchTarget))
+    }
 
 
     handleChange(event) {
         this.setState({searchTarget: event.target.value});
-    };
+    }
+
+
+    strip(mac){
+        let small = mac.toLowerCase();
+        let arr = small.split("");
+        let filtered = arr.filter((x)=>{
+            return (x >= 'a' && x <= 'f') || (x >= 0 && x <= 9 );
+        })
+        return filtered.join('');
+    }
+
+
+    validateMac(mac) {
+        if (~mac.search(/[^a-f0-9A-F\.:-]/)) return false;
+        if (this.strip(mac).length === 12) return true;
+        return false;
+    }
+
+
+    getWarning() {
+        if (this.validateMac(this.state.searchTarget)) {
+            return (<Button bsStyle="primary" bsSize="small" onClick={this.lookupMacHistory.bind(this)}>search</Button>)
+        } else {
+            return (
+                <span>
+                    {/*<Button bsStyle="default" bsSize="small">search</Button> &nbsp;&nbsp;*/}
+                    <span style={{color: 'red'}}>invalid mac address</span>
+                </span>
+            )
+        }
+
+    }
 
 
     render() {
-        this.lookupMacHistory = this.lookupMacHistory.bind(this);
+        //this.lookupMacHistory = this.lookupMacHistory.bind(this);
 
 
         return (
             <div>
                 <Navbar />
                 <div class="page-header">
-                    <h1>Search history <small>via manual input</small></h1>
+                    <h1>Search history
+                        <small> via manual input</small>
+                    </h1>
                 </div>
 
-                <span>Mac to look-up: </span>
+                <span>Mac to look-up: </span>&nbsp;&nbsp;
                 <input name="targetMac" value={this.state.searchTarget} type="text"
-                            onChange={this.handleChange.bind(this)}/>
-                <button class="btn btn-primary btn-xs" onClick={this.lookupMacHistory}>go</button>
+                       onChange={this.handleChange.bind(this)}/>&nbsp;&nbsp;
+                {this.getWarning()}
                 <br/>
                 <br/>
                 <br/>
@@ -57,8 +92,8 @@ export default class Search extends React.Component {
                 <h4>The easier way</h4>
 
                 <p>You can access the history of any device listed on the live monitor screen
-                by clicking on its image. On the scan screen there are links for all observed devices
-                which also directly bring you to the history of that device.</p>
+                    by clicking on its image. On the scan screen there are links for all observed devices
+                    which also directly bring you to the history of that device.</p>
                 <p>To return to the previous screen from history, click on the image on the history page.</p>
 
                 <br/>
